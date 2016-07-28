948e025e1cf9f82f38d53043eade9ce988b0636c
Toby Curtis <curti024@csusm.edu>
2016-07-27 18:12:44 -0700

    ui changes

--- app/app.js
+++ app/app.js
@@ -1,94 +1,94 @@
-//currentSessionOpen is the variable to show if we're in the middle of trying to match
-var currentSessionOpen = false;
-//initialize the prevousCard value for the checking of matches
-var previousCard = null;
-//initialize number of pairs to be 0
-var numPairs = 0;
-
-//this function adds the random letter cards we put into a stack and organizes them into the right rows and columns
-function createDeck() {
-    var rows = 6;
-    var cols = 6;
-    var key = createRandom();
-    var deck = {};
-    deck.rows = [];
-
-    // create each row
-    for (var i = 0; i < rows; i++) {
-        var row = {};
-        row.cards = [];
-
-        // creat each card in the row
-        for (var j = 0; j < cols; j++) {
-            var card = {};
-            card.isFaceUp = false;
-            card.item = key.pop();
-            row.cards.push(card);
-        }
-        deck.rows.push(row);
-    }
-    return deck;
-}
-
-//this function takes a hard-coded array of characters and picks characters randomly. It then puts two of each random character in another 
-//array, and removes the letter it picked from the choices so that the letter won't be duplicated as another match pair
-function createRandom() {
-    var matches = 18;
-    var pool = [];
-    var answers = [];
-    var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z'];
-
-    // create array for the total number of cards with the index mathing the value as a plceholder
-    for (var i = 0; i < matches * 2; i++) {
-        pool.push(i); // random numbers
-    }
-
-    // generate an array with the random letters
-    for (var n = 0; n < matches; n++) {
-        // grab random letter from array and remove that letter from the
-        // original array
-
-        //get random index of letter we want to grab
-        var randLetter = Math.floor((Math.random() * letters.length));
-
-        //grab the letter at the random index then remove that letter from the array of letters that we're starting from so we don't duplicate it
-        var letter = letters[randLetter];
-        removeByIndex(letters, randLetter);
-
-        // get random index for placeholder array
-        var randPool = Math.floor((Math.random() * pool.length));
-
-        //insert letter into answers from the pool spot we just figured
-        insertByIndex(answers, pool[randPool], letter);
-
-        // remove the pool spot from the possible future pool spots so we don't put another random card in the same place
-        removeByIndex(pool, randPool);
-
-        // repeat above process 
-        randPool = Math.floor((Math.random() * pool.length));
-        insertByIndex(answers, pool[randPool], letter);
-        removeByIndex(pool, randPool);
-    }
-    return answers;
-}
-
-// uses splice to remove from array
-function removeByIndex(arr, index) {
-    arr.splice(index, 1);
-}
-
-//uses splice to add to array
-function insertByIndex(arr, index, item) {
-    arr.splice(index, 0, item);
-}
-
-
-var app = angular.module('cards', ['ngAnimate']);
+var app = angular.module('cards', []);
 
 app.controller("CardController", function($scope, $timeout) {
 
     $scope.deck = createDeck();
     $scope.isGuarding = false;
+
+    //currentSessionOpen is the variable to show if we're in the middle of trying to match
+    var currentSessionOpen = false;
+    //initialize the prevousCard value for the checking of matches
+    var previousCard = null;
+    //initialize number of pairs to be 0
+    var numPairs = 0;
+
+    //this function adds the random letter cards we put into a stack and organizes them into the right rows and columns
+    function createDeck() {
+        var rows = 6;
+        var cols = 6;
+        var key = createRandom();
+        var deck = {};
+        deck.rows = [];
+
+        // create each row
+        for (var i = 0; i < rows; i++) {
+            var row = {};
+            row.cards = [];
+
+            // creat each card in the row
+            for (var j = 0; j < cols; j++) {
+                var card = {};
+                card.isFaceUp = false;
+                card.item = key.pop();
+                row.cards.push(card);
+            }
+            deck.rows.push(row);
+        }
+        return deck;
+    }
+
+    //this function takes a hard-coded array of characters and picks characters randomly. It then puts two of each random character in another 
+    //array, and removes the letter it picked from the choices so that the letter won't be duplicated as another match pair
+    function createRandom() {
+        var matches = 18;
+        var pool = [];
+        var answers = [];
+        var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z'];
+
+        // create array for the total number of cards with the index mathing the value as a plceholder
+        for (var i = 0; i < matches * 2; i++) {
+            pool.push(i); // random numbers
+        }
+
+        // generate an array with the random letters
+        for (var n = 0; n < matches; n++) {
+            // grab random letter from array and remove that letter from the
+            // original array
+
+            //get random index of letter we want to grab
+            var randLetter = Math.floor((Math.random() * letters.length));
+
+            //grab the letter at the random index then remove that letter from the array of letters that we're starting from so we don't duplicate it
+            var letter = letters[randLetter];
+            removeByIndex(letters, randLetter);
+
+            // get random index for placeholder array
+            var randPool = Math.floor((Math.random() * pool.length));
+
+            //insert letter into answers from the pool spot we just figured
+            insertByIndex(answers, pool[randPool], letter);
+
+            // remove the pool spot from the possible future pool spots so we don't put another random card in the same place
+            removeByIndex(pool, randPool);
+
+            // repeat above process 
+            randPool = Math.floor((Math.random() * pool.length));
+            insertByIndex(answers, pool[randPool], letter);
+            removeByIndex(pool, randPool);
+        }
+        return answers;
+    }
+
+    // uses splice to remove from array
+    function removeByIndex(arr, index) {
+        arr.splice(index, 1);
+    }
+
+    //uses splice to add to array
+    function insertByIndex(arr, index, item) {
+        arr.splice(index, 0, item);
+    }
+
 
     $scope.guard = function(card) {
         if (!$scope.isGuarding)
@@ -111,7 +111,7 @@ app.controller("CardController", function($scope, $timeout) {
             currentSessionOpen = false;
             //turn the cards back over after we have seen the match fail
             $timeout(function() {
-                
+
                 previousCard.isFaceUp = card.isFaceUp = false;
                 previousCard = card;
                 $scope.isGuarding = false;
